cf78fa94922a712dfd17c93c1082c845cc745fa7
Jeremy <15869294164@163.com>
2017-11-23 19:07:19 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@
  * Created by apple on 2017/11/22.
  */
 exports.dbConf = {
-    host: 'rm-uf68q7wt3x80g0018o.mysql.rds.aliyuncs.com',
-    database: 'chatroom',
+    host: 'localhost',
+    database: 'tets',
     user: 'root',
-    password: 'Tjh19971121',
+    password: '',
     protocol: 'mysql',
     port: '3306',
     query: {pool: true}
